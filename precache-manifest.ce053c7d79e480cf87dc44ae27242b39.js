59c501226ecfb1ef329e739fc7a3e70e39222d50
Mikecoon <mishakozlov74@gmail.com>
2020-02-06 17:38:25 +0300

    Updates

---
+++ precache-manifest.ce053c7d79e480cf87dc44ae27242b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f18e1099c584a3477847dba387aea6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c21401dcc59b74986e",
+    "url": "/static/css/main.7f2bbbcf.chunk.css"
+  },
+  {
+    "revision": "56c795ade577e197272b",
+    "url": "/static/js/2.43e66a1a.chunk.js"
+  },
+  {
+    "revision": "837887ecd21269c4ac52238a3fc010b5",
+    "url": "/static/js/2.43e66a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c21401dcc59b74986e",
+    "url": "/static/js/main.4f318289.chunk.js"
+  },
+  {
+    "revision": "6b1599dbaba338edc494",
+    "url": "/static/js/runtime-main.ee73c4fd.js"
+  }
+]);
